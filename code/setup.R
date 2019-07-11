ee8d7a23d7f200d6a2b4d7dcedeb12e286fabbeb
Noushin Nabavi <nabavinoushin@gmail.com>
2019-07-11 16:17:38 -0700

    initialize folders/scripts

---
+++ code/setup.R
@@ -0,0 +1,9 @@
+
+
+# install/load packages and dependencies
+
+packages <- c("here", "data.table", "tidyverse", "tabplot", "RColorBrewer", "MASS", "pheatmap",
+              "leaflet", "rgdal", "ggridges", "plotly", "ggplot2", "lubridate",  "bcdata",
+              "shiny", "lattice", "mapview", "htmltools", "DT", "sf", "bcmaps", "DESeq2")
+
+lapply(packages, library, character.only = TRUE)
