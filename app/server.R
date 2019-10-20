54fb4004920bdc2ed589a1f35b9125f5a4650d3e
Casey Engstrom <cengstro@sfu.ca>
2019-10-20 14:31:35 -0700

    server now works with limma interactive plot

--- app/server.R
+++ app/server.R
@@ -4,6 +4,7 @@ library(shiny)
 library(tidyverse)
 library(here)
 library(ggthemes)
+library(limma)
 
 
 
@@ -15,9 +16,12 @@ shinyServer(function(input, output, session) {
     strain_meta <- read_csv("data/strain_meta.csv")
     go_annotation <- read_csv("data/go_annotation.csv")
     umap_df <- read_csv("data/umap.csv")
-    limma_group <- read_csv("data/limma_grouping_table.csv")
-    limma_expr <- read_csv("data/04_SC_expression.csv")
-    
+    group_table <- read_csv("data/limma_grouping_table.csv")
+    expr_mat <- read_csv("data/04_SC_expression.csv")
+    group_table <- group_table[!duplicated(group_table$ID),]
+    gene_symbols <- expr_mat$gene
+    expr_mat <- as.matrix(expr_mat[,colnames(expr_mat) != "gene"])
+    rownames(expr_mat) <- gene_symbols
     
     #Find the GO domain selected and change the options on the response checkboxes for the Heatmap Panel
     observe({
@@ -150,66 +154,96 @@ shinyServer(function(input, output, session) {
     })
     
     output$limma <- renderPlot({
-     
-      plot(x=1:5, y=2:10)
+    
+        
+        group1 <- input$group1
+        group2 <- input$group2
+        
+        checkGroupNames <- function(group1, group2) {
+            ## This code will ensure that the submitted groups for comparison in limma
+            ## are valid. Groups cannot overlap in any manner, and "SI" cannot exist
+            ## as a standalone group. Refer to group table for the acceptable group names.
+            group1 <- group1[order(group1)]
+            group2 <- group2[order(group2)]
+            if (identical(group1, group2)) {
+                stop("Groups must be unique")
+            }
+            if (group1 == "SI" || group2 == "SI") {
+                stop("SI only has one sample, cannot use as group")
+            }
+            if (any(group1 %in% group2)) {
+                stop("Groups cannot share any samples")
+            }
+        }
+       
+        
+
+        matchSamples <- function(group1, group2) {
+            ## Subset the samples to match the submitted groups
+            groups <- c(group1, group2)
+            matched_samples <- group_table %>%
+                filter(Group %in% groups) %>%
+                select(ID, Group)
+        }
+        
+
+       getDiffExpressedResults <- function(group1, group2) {
+        ## Assumes that the expression matrix and metadata table have already been defined.
+
+        checkGroupNames(group1, group2)
+        matched_samples <- matchSamples(group1, group2)
+
+        subset_matrix <- expr_mat[, which(colnames(expr_mat) %in% matched_samples$ID)]
+        design_mat <- model.matrix(~matched_samples$Group)
+        lm_fit <- eBayes(lmFit(subset_matrix, design_mat))
+
+        summary_table <- topTable(
+          lm_fit,
+          adjust = "fdr",
+          sort.by = "B",
+          number = Inf,
+          genelist = gene_symbols
+        )
+      }
+
+      results_table <- getDiffExpressedResults(group1, group2)
       
-      
-      #  getDiffExpressedResults <- function(group1, group2) {
-      #   ## Assumes that the expression matrix and metadata table have already been defined.
-      #   
-      #   checkGroupNames(group1, group2)
-      #   matched_samples <- matchSamples(group1, group2)
-      #   
-      #   subset_matrix <- expr_mat[, which(colnames(expr_mat) %in% matched_samples$ID)]
-      #   design_mat <- model.matrix(~matched_samples$Group)
-      #   lm_fit <- eBayes(lmFit(subset_matrix, design_mat))
-      #   
-      #   summary_table <- topTable(
-      #     lm_fit,
-      #     adjust = "fdr",
-      #     sort.by = "B",
-      #     number = Inf,
-      #     genelist = gene_symbols
-      #   )
-      # }
-      # 
-      # results_table <- getDiffExpressedResults("AF", "QN")
-      #  
-      # plotVolcano <- function(results_table) {
-      #   ## Expects the table returned by getDiffExpressedResults
-      #   
-      #   
-      #   # Assign binary vector to determine if results are significant for coloring
-      #   results_table$Color <- rep(0, nrow(results_table))
-      #   for (i in 1:nrow(results_table)) {
-      #     if (abs(results_table$logFC[i]) > 2 &&
-      #         results_table$adj.P.Val[i] < 0.05) {
-      #       results_table$Color[i] <- 1
-      #     }
-      #   }
-      #   
-      #   g <-
-      #     ggplot(results_table, aes(
-      #       x = logFC,
-      #       y = -log10(adj.P.Val),
-      #       color = as.factor(Color)
-      #     )) +
-      #     geom_point(alpha = 0.6, size = 0.8) +
-      #     theme_classic() +
-      #     geom_hline(yintercept = -log10(0.05), linetype = "dotted") +
-      #     geom_vline(xintercept = 2, linetype = "dotted") +
-      #     geom_vline(xintercept = -2, linetype = "dotted") +
-      #     xlab("Log fold change") +
-      #     ylab("Significance") +
-      #     scale_color_manual(values = c("black", "red")) +
-      #     theme(legend.position = "none",
-      #           text = element_text(size=20))
-      #   
-      #   return (g)
-      # }
-      # 
-      # g
-      
+
+      plotVolcano <- function(results_table) {
+        ## Expects the table returned by getDiffExpressedResults
+
+
+        # Assign binary vector to determine if results are significant for coloring
+        results_table$Color <- rep(0, nrow(results_table))
+        for (i in 1:nrow(results_table)) {
+          if (abs(results_table$logFC[i]) > 2 &&
+              results_table$adj.P.Val[i] < 0.05) {
+            results_table$Color[i] <- 1
+          }
+        }
+
+        g <-
+          ggplot(results_table, aes(
+            x = logFC,
+            y = -log10(adj.P.Val),
+            color = as.factor(Color)
+          )) +
+          geom_point(alpha = 0.6, size = 0.8) +
+          theme_classic() +
+          geom_hline(yintercept = -log10(0.05), linetype = "dotted") +
+          geom_vline(xintercept = 2, linetype = "dotted") +
+          geom_vline(xintercept = -2, linetype = "dotted") +
+          xlab("Log fold change") +
+          ylab("Significance") +
+          scale_color_manual(values = c("black", "red")) +
+          theme(legend.position = "none",
+                text = element_text(size=20))
+
+        return (g)
+      }
+
+      plotVolcano(results_table)
+
       
     })
     
