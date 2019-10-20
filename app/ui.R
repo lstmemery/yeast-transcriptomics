9a66c965d46ade8658ee0ce7594e2fdaa84f7918
Siddarth <sidr97@gmail.com>
2019-10-20 10:01:48 -0700

    Fix Heatmap Tab
    Fixed the difficulties in the variables. The issue was that I was hunting for input variables using the input$, but I should have remembered that some variables don't have everything.

--- app/ui.R
+++ app/ui.R
@@ -11,96 +11,50 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
     titlePanel(HTML("<em>S. cervisiae</em> RNA expression")),
-
+    
     hr(),
-
+    
     tabsetPanel(
         tabPanel("Heatmap",
-                fluidRow(
-
-                    # Sidebar with a slider input for number of bins
-                    sidebarLayout(
-                        position = "left",
-                        sidebarPanel(
-                            fluidRow(
-                                column(5,
-                                    selectInput(inputId="go_domain_heatmap",
-                                                label = "GO domain",
-                                                choices = list("Biological process",
-                                                           "Cellular component",
-                                                           "Molecular function"),
-                                                selected = "Biological process"
-                                                ),
-                                ),
-                                column(5,
-                                    selectInput(inputId="strain_tag_type_heatmap",
-                                                label = "Strain metadata",
-                                                choices = list("primary",
-                                                               "secondary",
-                                                               "additional_information"),
-                                                selected = "primary"
-                                                ),
-                                )
-                            ),
-                            fluidRow(
-                                column(5,
-                            checkboxGroupInput(inputId = "inCheckboxGroup_heatmap",
-                                               label = "Input checkbox",
-                                               c())
-
-                                )
-                            )
-                        ),
-
-                        # Show a plot of the generated distribution
-                        mainPanel(br(),plotOutput(outputId="heat",height = 600))
-                    )
-                )),
-                tabPanel("UMAP",
-                         fluidRow(
-
-                             # Sidebar with a slider input for number of bins
-                             sidebarLayout(
-                                 position = "left",
-                                 sidebarPanel(
-                                     fluidRow(
-                                         column(5,
-                                                selectInput(inputId="go_domain_UMAP",
-                                                            label = "GO domain",
-                                                            choices = list("Biological process",
-                                                                           "Cellular component",
-                                                                           "Molecular function"),
-                                                            selected = "Biological process"
-                                                ),
-                                         ),
-                                         column(5,
-                                                selectInput(inputId="strain_tag_type_UMAP",
-                                                            label = "Strain metadata",
-                                                            choices = list("primary",
-                                                                           "secondary",
-                                                                           "additional_information"),
-                                                            selected = "primary"
-                                                ),
-                                         )
-                                     ),
-                                     fluidRow(
-                                         column(5,
-                                                selectInput(inputId = "response_UMAP",
-                                                                   label = "Input checkbox",
-                                                                   c())
-
-                                         )
-                                     )
-                                 ),
-
-                                 # Show a plot of the generated distribution
-                                 mainPanel()
-                             )
-                         )),
-                tabPanel("PCA")
-                )
+                 fluidRow(
+                     
+                     # Sidebar with a slider input for number of bins
+                     sidebarLayout(
+                         position = "left",
+                         sidebarPanel(
+                             selectInput(inputId="go_domain_heatmap",
+                                         label = "GO domain",
+                                         choices = list("Biological process",
+                                                        "Cellular component", 
+                                                        "Molecular function"),
+                                         selected = "Biological process"
+                             ),
+                             selectInput(inputId="strain_tag_type_heatmap",
+                                         label = "Strain metadata",
+                                         choices = list("primary",
+                                                        "secondary", 
+                                                        "additional_information"),
+                                         selected = "primary"
+                             ),
+                             selectInput(inputId="order_by_heatmap",
+                                         label = "Order GO by:",
+                                         choices = c()
+                             ),
+                             checkboxGroupInput(inputId = "inCheckboxGroup_heatmap", 
+                                                label = "Input checkbox",
+                                                c())
+                         ),
+                         
+                         # Show a plot of the generated distribution
+                         mainPanel(plotOutput(outputId="heat"))
+                     )
+                 )),
+        tabPanel("UMAP"),
+            
+        tabPanel("PCA")
     )
+)
 )
